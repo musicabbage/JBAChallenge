d1ddf69dc7fe018e62ef51dcea30402130e39706
szuyun <cabbageliang@gmail.com>
2023-10-16 22:17:09 +0100

    feat(#4): add function to parse header

--- JBAChallenge/Root/RootViewModel.swift
+++ JBAChallenge/Root/RootViewModel.swift
@@ -23,6 +23,33 @@ class RootViewModel: RootViewModelProtocol {
     }
 }
 
+private extension RootViewModel {
+    
+    func scan(headerString: String) throws -> [String: String] {
+        let scanner = Scanner(string: headerString)
+        scanner.charactersToBeSkipped = ["="]
+        var result: [String: String] = [:]
+        var currentKey = ""
+        
+        while !scanner.isAtEnd {
+            print(scanner.currentIndex)
+            guard currentKey.isEmpty else {
+                if let value = scanner.scanUpToString("]") {
+                    result[currentKey] = value
+                    currentKey = ""
+                }
+                continue
+            }
+
+            guard scanner.scanUpToString("[") == nil else { continue }
+            guard let key = scanner.scanUpToString("=") else { continue }
+            
+            currentKey = String(key.dropFirst())
+        }
+        return result
+    }
+}
+
 class MockRootViewModel: RootViewModelProtocol {
     func readFile(url: URL) {
         
