c1fbf35b94222725326028bfd2372f57dd1cf8e4
szuyun <cabbageliang@gmail.com>
2023-10-16 22:21:42 +0100

    feat(#5): add functions to parse grid-ref and grid value

---
+++ JBAChallenge/Common/Errors.swift
@@ -0,0 +1,12 @@
+//
+//  Errors.swift
+//  JBAChallenge
+//
+//  Created by cabbage on 2023/10/13.
+//
+
+import Foundation
+
+enum Error: Swift.Error {
+    case invalidGridRef
+}
--- JBAChallenge/Root/RootViewModel.swift
+++ JBAChallenge/Root/RootViewModel.swift
@@ -48,6 +48,49 @@ private extension RootViewModel {
         }
         return result
     }
+    
+    func findGrid(string: String) throws -> (x: Int, y: Int)? {
+        print(string)
+        let scanner = Scanner(string: string)
+        scanner.charactersToBeSkipped = CharacterSet(charactersIn: "=, ")
+
+        guard let key = scanner.scanUpToString("="),
+              key == "Grid-ref" else {
+            return nil
+        }
+        
+        var x: Int!
+        var y: Int!
+        while !scanner.isAtEnd {
+            guard let refValue = scanner.scanInt() else { continue }
+            if x == nil {
+                x = refValue
+            } else {
+                y = refValue
+            }
+        }
+        
+        if x == nil || y == nil {
+            throw Error.invalidGridRef
+        }
+        
+        return (x, y)
+    }
+    
+    func findNumbers(string: String) -> [Int] {
+        var result: [Int] = []
+        
+        let scanner = Scanner(string: string)
+        
+        guard var value = scanner.scanInt() else { return result }
+        result.append(value)
+        while !scanner.isAtEnd {
+            if let value = scanner.scanInt() {
+                result.append(value)
+            }
+        }
+        return result
+    }
 }
 
 class MockRootViewModel: RootViewModelProtocol {
